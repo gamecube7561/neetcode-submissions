f8d311c93df10c554c97fa18c5ca868ea87d504f
Jack Kennedy <jack.f.kennedy2026@gmail.com>
2026-07-06 11:57:33 -0400

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        finalset = []
+        seen = tuple()
+        nums.sort()
+
+        def helper(i, nums, currset, finalset):
+            if i >= len(nums):
+                finalset.append(currset.copy())
+                return
+
+            currset.append(nums[i])
+            helper(i + 1, nums, currset, finalset)
+            currset.pop()
+            while i < len(nums)-1 and nums[i] == nums[i+1]:
+                i += 1
+            helper(i + 1, nums, currset, finalset)
+
+        helper(0, nums, [], finalset)
+        return finalset
