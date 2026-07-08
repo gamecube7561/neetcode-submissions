4abbd757f25da9e8f12fa1e9649bafb5b86c3c94
Jack Kennedy <jack.f.kennedy2026@gmail.com>
2026-07-08 05:28:54 -0400

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(31):
+            res += (n & 1)
+            res = res << 1
+            n = n >> 1
+        res += (n & 1)
+        return res
